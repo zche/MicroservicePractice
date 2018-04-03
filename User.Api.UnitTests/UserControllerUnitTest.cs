e747ed4739045eb55ac904ca4b57833e5a8ccc07
zche <peterche123@163.com>
2018-04-03 15:43:44 +0800

    单元测试

--- User.Api.UnitTests/UserControllerUnitTest.cs
+++ User.Api.UnitTests/UserControllerUnitTest.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FluentAssertions;
 using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace User.Api.UnitTests
 {
@@ -55,6 +57,50 @@ namespace User.Api.UnitTests
             var user = await userContext.Users.FirstOrDefaultAsync(u => u.Id == 1);
             user.Should().NotBeNull();
             user.Name.Should().Be("che");
+        }
+
+
+        [Fact]
+        public async Task Patch_ReturnNewProperties_WithNewProperty()
+        {
+            (UserController userController, Data.UserContext userContext) = this.GetUserController();
+            var doc = new JsonPatchDocument<Models.AppUser>();
+            doc.Replace(u => u.Properties, new List<Models.UserProperty>
+            { new Models.UserProperty
+            {
+                Key ="fin_industry",
+                Value="Mongo",
+                Text ="Mongo"
+            }
+            });
+            var resp = await userController.Patch(doc);
+            var result = resp.Should().BeOfType<JsonResult>().Subject;
+            var appUser = result.Value.Should().BeAssignableTo<Models.AppUser>().Subject;
+            appUser.Properties.Count.Should().Be(1);
+            appUser.Properties.First().Value.Should().Be("Mongo");
+            appUser.Properties.First().Key.Should().Be("fin_industry");
+
+            var user = await userContext.Users.FirstOrDefaultAsync(u => u.Id == 1);
+            user.Properties.Count.Should().Be(1);
+            user.Properties.First().Value.Should().Be("Mongo");
+            user.Properties.First().Key.Should().Be("fin_industry");
+        }
+
+
+
+        [Fact]
+        public async Task Patch_ReturnNewProperties_WithRemvoeProperty()
+        {
+            (UserController userController, Data.UserContext userContext) = this.GetUserController();
+            var doc = new JsonPatchDocument<Models.AppUser>();
+            doc.Replace(u => u.Properties, new List<Models.UserProperty>());
+            var resp = await userController.Patch(doc);
+            var result = resp.Should().BeOfType<JsonResult>().Subject;
+            var appUser = result.Value.Should().BeAssignableTo<Models.AppUser>().Subject;
+            appUser.Properties.Count.Should().Be(0);
+
+            var user = await userContext.Users.FirstOrDefaultAsync(u => u.Id == 1);
+            user.Properties.Should().BeEmpty();
         }
     }
 }
